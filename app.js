b470feed16ffb94697bf0dd60f5825d9fa5d4039
lvarnum <lvarnum@email.arizona.edu>
2021-03-05 15:55:23 -0700

    Added files

---
+++ app.js
@@ -0,0 +1,7 @@
+let start = document.getElementById("start");
+let form = document.querySelector(".popup");
+
+start.onclick = () => {
+    start.style.display = "none";
+    form.style.display = "block";
+}
